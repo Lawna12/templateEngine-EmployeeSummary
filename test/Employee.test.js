1f5df9e8763220a0bbe11e6f3f75e5cb7ca8b835
AndrewL <lawna12@gmail.com>
2020-02-10 21:58:07 -0500

    updated all js

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -8,25 +8,25 @@ test("Can instantiate Employee instance", () => {
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
-  expect(e._name).toBe(name);
+  expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
-  expect(e._id).toBe(testValue);
+  expect(e.id).toBe(testValue);
 });
 
 test("Can set title via constructor arguments", () => {
   const testValue = "title";
   const e = new Employee("Foo", 1, testValue);
-  expect(e._name).toBe(name);
+  expect(e.name).toBe(name);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, "title", testValue);
-  expect(e._email).toBe(testValue);
+  expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
